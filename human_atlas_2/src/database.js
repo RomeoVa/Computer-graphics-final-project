0bcb3667d023c1f44c1b8d8cae2acbb758aa8d04
maupeon <maupeon@gmail.com>
2019-12-02 11:05:05 -0600

    Showing just groups of models

---
+++ human_atlas_2/src/database.js
@@ -0,0 +1,43 @@
+     
+var boneInfo =[];
+var muscleInfo =[];
+boneInfo[0] = '<h1>Humerus</h1>' +
+'<p>The humerus is a long bone in the arm that runs from the shoulder to the elbow.</p>';
+boneInfo[1] = '<h1>Radius</h1>' +
+'<p>The radius or radial bone is one of the two large bones of the forearm, the other being the ulna.</p>';
+boneInfo[2] = '<h1>Ulna</h1>' +
+'<p>The ulna is a long bone found in the forearm that stretches from the elbow to the smallest finger, and when in anatomical position, is found on the medial side of the forearm. </p>';
+
+
+muscleInfo[0] = '<h1>Flexor retinaculum of  wrist</h1>' +
+'<p>The flexor retinaculum (transverse carpal ligament, or anterior annular ligament) is a fibrous band on the palmar side of the hand near the wrist. </p>';
+muscleInfo[1]= '<h1>Humeral head of flexor carpi ulnaris</h1>' +
+'<p>The flexor carpi ulnaris muscle (or FCU) is a muscle of the human forearm that acts to flex and adduct (medial deviation) the hand.</p>';
+muscleInfo[2] = '<h1>Humeral head of pronator teres</h1>' +
+'<p>The pronator teres muscle is located on the palmar side of the forearm, below the elbow.</p>';
+muscleInfo[3] = '<h1>Long head of biceps brachii</h1>' +
+'<p>The biceps brachii, sometimes known simply as the biceps, is a skeletal muscle that is involved in the movement of the elbow and shoulder. </p>';
+muscleInfo[4]= '<h1>Long head of triceps brachii</h1>' +
+'<p>The long head of the Triceps brachii is one of three heads that make up the muscle. It descends between teres minor and major, dividing the wedge-shaped interval between them and the humerus into triangular and quadrangular spaces.</p>';
+muscleInfo[5] = '<h1>Medial head of triceps brachii</h1>' +
+'<p>The triceps brachii is the muscle that runs down the back of the humerus, which is the long bone of the upper arm, and ends at the top of the ulna, which is the long bone of the forearm.</p>';
+muscleInfo[6] = '<h1>Right brachialis</h1>' +
+'<p>The brachialis (brachialis anticus) is a muscle in the upper arm that flexes the elbow joint.</p>';
+muscleInfo[7] = '<h1>Right brachioradialis.</h1>' +
+'<p>The brachioradialis is a muscle of the forearm that flexes the forearm at the elbow.</p>';
+muscleInfo[8] = '<h1>Extensor carpi radialis brevis.</h1>' +
+'<p>In human anatomy, extensor carpi radialis brevis is a muscle in the forearm that acts to extend and abduct the wrist. </p>';
+muscleInfo[9] = '<h1>Extensor carpi radialis longus</h1>' +
+'<p>A long muscle on the radial side of the back of the forearm that extends and abducts the hand</p>';
+muscleInfo[10] = '<h1>Flexor carpi radialis</h1>' +
+'<p>In anatomy, flexor carpi radialis is a muscle of the human forearm that acts to flex and (radial) abduct the hand. </p>';
+muscleInfo[11] = '<h1>Flexor digitorum superficialis</h1>' +
+'<p>Flexor digitorum superficialis (flexor digitorum sublimis) is an extrinsic flexor muscle of the fingers at the proximal interphalangeal joints.</p>';
+muscleInfo[12] = '<h1>Palmaris longus.</h1>' +
+'<p>A superficial muscle of the forearm lying on the medial side of the flexor carpi radialis that arises especially from the medial epicondyle of the humerus, inserts especially into the palmar aponeurosis, and acts to flex the hand</p>';
+muscleInfo[13] = '<h1>Short head of biceps brachii</h1>' +
+'<p>The short head of the biceps brachii is the shorter and medial of the two bodies that form the biceps brachii muscle in the upper arm. Like the long head of the biceps brachii, the short head is a flexor and supinator of the elbow joint. At the shoulder joint, the short head aids in adduction of the humerus.</p>';
+muscleInfo[14] = '<h1>Ulnar head of flexor carpi ulnaris.</h1>' +
+'<p>The ulnar head arises from the medial margin of the olecranon of the ulna and from the upper two-thirds of the dorsal border of the ulna by an aponeurosis.</p>';
+muscleInfo[15] = '<h1>Ulnar head of pronator teres.</h1>' +
+'<pThe pronator teres is a muscle (located mainly in the forearm) that, along with the pronator quadratus, serves to pronate the forearm (turning it so that the palm faces posteriorly when from the anatomical position).p>';
